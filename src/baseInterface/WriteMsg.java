b43103750276b4d09b97e16709b37562faf42561
hossam fawzy <eng.hossam.fawzy.elsafty@gmail.com>
2019-05-05 18:06:16 +0200

    implement write in  Master

--- src/baseInterface/WriteMsg.java
+++ src/baseInterface/WriteMsg.java
@@ -4,5 +4,28 @@ public class WriteMsg {
 	private long transactionId;
 	private  long timeStamp;
 	private ReplicaLoc loc;
+	public WriteMsg(long transactionId,long timeStamp , ReplicaLoc loc){
+		this.loc = loc;
+		this.timeStamp = timeStamp;
+		this.transactionId = transactionId;
+	}
+	public long getTransactionId() {
+		return transactionId;
+	}
+	public void setTransactionId(long transactionId) {
+		this.transactionId = transactionId;
+	}
+	public long getTimeStamp() {
+		return timeStamp;
+	}
+	public void setTimeStamp(long timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	public ReplicaLoc getLoc() {
+		return loc;
+	}
+	public void setLoc(ReplicaLoc loc) {
+		this.loc = loc;
+	}
 
 }
--- src/server/MasterServer.java
+++ src/server/MasterServer.java
@@ -4,9 +4,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 import java.util.Scanner;
-
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import baseInterface.FileContent;
 import baseInterface.MasterServerClientInterface;
@@ -14,20 +17,22 @@ import baseInterface.ReplicaLoc;
 import baseInterface.WriteMsg;
 
 public class MasterServer implements MasterServerClientInterface{
-	private HashMap<String, ReplicaLoc[]> fileRepLocaMap;
+	private ConcurrentHashMap<String, ReplicaLoc[]> fileRepLocaMap;
+	ConcurrentHashMap<String, ReplicaLoc> nameReplicaLocMap = new ConcurrentHashMap<>();
+	private static Random rand = new Random();
+	private ArrayList<ReplicaLoc> replcasLoc = new ArrayList<>();
 	private static String METADATA_FILE_NAME = "metadata.txt";
 	private static String REPLICA_FILE_NAME = "replicaServers.txt";
 	private static int REP_PER_FILE = 2;
-	
-	public MasterServer() throws FileNotFoundException {
+	private AtomicInteger transID ,timeStamp;
+	public MasterServer(String direction) throws FileNotFoundException {
 		File metaData = new File(METADATA_FILE_NAME);
 		File repServers = new File(REPLICA_FILE_NAME);
-
-		HashMap<String, ReplicaLoc> nameReplicaLocMap = new HashMap<>();
-
-
+		nameReplicaLocMap = new ConcurrentHashMap<>();
+		fileRepLocaMap = new ConcurrentHashMap<>();
         Scanner sc = new Scanner(repServers);
-
+        Scanner sc2 = new Scanner(metaData);
+        File dir = new File(direction);
         while (sc.hasNextLine()) {
             String line = sc.nextLine();
             String[] splited = line.split("\\s+");
@@ -35,12 +40,18 @@ public class MasterServer implements MasterServerClientInterface{
             String ip = splited[1];
             int port = Integer.parseInt(splited[2]);
             nameReplicaLocMap.put(name, new ReplicaLoc(name,ip,port));
-
         }
         sc.close();
-        
-        Scanner sc2 = new Scanner(metaData);
+    	replcasLoc = (ArrayList<ReplicaLoc>) nameReplicaLocMap.values();	
+        if (dir.mkdirs()) {
+            System.out.format("Directory %s has been created.", dir.getAbsolutePath());
 
+        } else if (dir.isDirectory()) {
+            System.out.format("Directory %s has already been created.", dir.getAbsolutePath());
+
+        } else {
+            System.out.format("Directory %s could not be created.", dir.getAbsolutePath());
+        }
         while (sc2.hasNextLine()) {
             String line = sc2.nextLine();
             String[] splited = line.split("\\s+");
@@ -65,8 +76,33 @@ public class MasterServer implements MasterServerClientInterface{
 
 	@Override
 	public WriteMsg write(FileContent data) throws RemoteException, IOException {
-		// TODO Auto-generated method stub
-		return null;
+		String fileName = data.getFileName();
+		int tID = transID.incrementAndGet();
+		int timestamp = timeStamp.incrementAndGet();
+		ReplicaLoc[] replicas = null;
+		if(!fileRepLocaMap.containsKey(fileName)){
+			replicas = getRandomReplica();
+		}
+		else {
+			replicas = fileRepLocaMap.get(fileName);
+		}
+		fileRepLocaMap.put(fileName, replicas);
+		ReplicaLoc primaryLoc = replicas[0];
+		
+		
+		return new WriteMsg(tID, timestamp, primaryLoc);
+	}
+	public ReplicaLoc[] getRandomReplica(){
+		ReplicaLoc[] replcas = new ReplicaLoc[REP_PER_FILE];
+		boolean[] visited = new boolean[replcasLoc.size()];
+		for (int i = 0; i < replcas.length; i++) {
+			int randomReplica = rand.nextInt(replcasLoc.size());
+			while (visited[randomReplica])
+				randomReplica = rand.nextInt(replcasLoc.size());
+			visited[randomReplica] = true;
+			replcas[i] = replcasLoc.get(i);
+		}
+		return replcas;
 	}
 	public static void main(String[] args) throws FileNotFoundException {
 		String masterName = "masterServer";
